3b6f77d6657239a44ab961289637ffdf091109f6
Gaston Seneza <gastonseneza@tmcfs-macbook-pro-3.local>
2016-05-02 12:10:50 -0500

    Initial Commit

---
+++ FunctionsPrctice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FunctionsPrctice
+//
+//  Created by Gaston Seneza on 5/2/16.
+//  Copyright © 2016 Gaston Seneza. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
